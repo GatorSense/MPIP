75f5a583aa430f206504af630ff87e26bcc67786
aseethepalli <aseethepalli@noble.org>
2018-04-02 11:51:52 -0500

    Readme.txt renamed and updated sources.

--- statistical analysis/analyze_2016.r
+++ statistical analysis/analyze_2016.r
@@ -27,6 +27,9 @@ library(tidyr)
 library(grid)
 library(gridExtra)
 
+# multiplot function
+# Source:
+# http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     library(grid)
     plots <- c(list(...), plotlist)
